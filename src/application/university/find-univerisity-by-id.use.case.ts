d4d665f74c4fbfd86233e3315051fbc8534fdfbc
@Alnkle621 <kaiquemendesilva82@gmail.com>
2023-06-10 21:51:43 -0300

    feat: add find university use case

--- src/application/university/find-univerisity-by-id.use.case.ts
+++
@@ -1,8 +0,0 @@
-import { FindUniversityByIdAdapter } from './interfaces/find-university-by-id.interfaces';
-import { Response_ } from '../../domain/error/custom.error';
-
-export class FindUniverisityByIdUseCase implements FindUniversityByIdAdapter {
-  Invoke(input: string): Promise<Response_> {
-    return Promise.resolve(undefined);
-  }
-}
